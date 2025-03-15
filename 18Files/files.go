b761a4bd518aaf91d258cb6e9e1f3840bc32d549
Hifzu04 <hifzurbtech@gmail.com>
2025-03-16 00:26:45 +0530

    reading using os.ReadFile()

--- 18Files/files.go
+++ 18Files/files.go
@@ -3,28 +3,45 @@ package main
 import (
 	"fmt"
 	"io"
+
 	"os"
 )
 
+func main() {
+	content := "this is my content that needs to in a  file , can i modify that"
 
-func main(){
-	content := "this is my content that needs to in a file"
+	//creating a file
+	file, err := os.Create("./myfile.txt")
 
-	//creating a file 
-	file  ,err := os.Create("./myfile.txt")
-
-	if err!= nil {
+	if err != nil {
 		panic(err)
 	}
 
 	//writting something into the created file
 	//io.WriteString writes in  file a returns length of the content
-	
-	mylen , _ :=io.WriteString(file , content)
+
+	mylen, _ := io.WriteString(file, content)
+
+	//return the length of content in the file
 	fmt.Println(mylen)
 
 	defer file.Close()
 
+	//reading content from created file
+	//io.Readfile() reads content in databyte instead of string format
 
-	
+	mydataByte, err := os.ReadFile("./myfile.txt")
+	checkNilErr(err)
+	// if err!= nil {
+	// 	panic(err)
+	// }
+
+	fmt.Println(string(mydataByte))
+
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
