26f159e70214a3f5db034ab535c8af5a9836ce95
Hifzu04 <hifzurbtech@gmail.com>
2025-03-28 16:40:15 +0530

    all about module

---
+++ 25Module/go.mod
@@ -0,0 +1,5 @@
+module github.com/Hifzu04/mymodule
+
+go 1.24.0
+
+require github.com/gorilla/mux v1.8.1
---
+++ 25Module/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Welcome to go module")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome) 
+
+	//http.ListenAndServe(":4000" ,r)
+	log.Fatal(http.ListenAndServe(":4000" ,r))
+}
+
+func greeter() {
+	fmt.Println("hey there mod useres")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>welecome to golang form respose by hifzur</h1>"))
+}
